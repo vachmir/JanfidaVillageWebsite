4595316c5a8f079cbd59e8fc7b9ab2ec0c1d2266
vchgn <vchgn@VACHMIRLAPTOP>
2021-11-03 04:01:03 +0400

    Add project files.

---
+++ JanfidaVillageWebsite/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JanfidaVillageWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Agriculture()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Description()
+        {
+            return View();
+        }
+        public ActionResult Places()
+        {
+            return View();
+        }
+
+    }
+}
